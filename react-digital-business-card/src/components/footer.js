79df514c079129ff683d961096b910348d4809b4
roman <51534566+roman-thapa@users.noreply.github.com>
2023-07-18 13:10:41 +0530

    Style footer properly

--- react-digital-business-card/src/components/footer.js
+++ react-digital-business-card/src/components/footer.js
@@ -4,11 +4,11 @@ export default function Footer() {
             <a href="https://github.com/roman-thapa">
                 <img src= "./git-logo.png" alt="github logo" width="40px" />
             </a>
-            <a href="https://twitter.com/roman_starboy">
-                <img src= "./twitter-logo.png" alt="twitter logo" width="40px" />
-            </a>
             <a href="https://www.linkedin.com/in/roman-thapa-120223191/">
                 <img src= "./linkedin-logo.png" alt="linkedin logo" width="40px" />
+            </a>
+            <a href="https://twitter.com/roman_starboy">
+                <img src= "./twitter-logo.png" alt="twitter logo" width="40px" />
             </a>
         </div>
     )
